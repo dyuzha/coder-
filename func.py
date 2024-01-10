162b2f214285a2640d5f13c90bb270c8ecab127e
матвей Дюжев <dyuzha@matvejs-MacBook-Pro.local>
2024-01-11 01:32:23 +0300

    modified file

--- func.py
+++ func.py
@@ -1,5 +1,6 @@
 import random as r
 
+
 def randArr(n: int, float = False, start = -99, end = 99) -> list:
     if float:
         arr = [r.uniform(start, end) for i in range(n)]
@@ -7,6 +8,23 @@ def randArr(n: int, float = False, start = -99, end = 99) -> list:
         arr = [r.randint(start, end) for i in range(n)]
     return arr
 
-arr = randArr(10, float=True)
 
-print(arr)
+def insertionSort(arr: list, reverse=True) -> None:
+    n = len(arr)
+    
+    for i in range(1, n):
+        j = i - 1
+        flag = 0
+        while j >= 0 and flag == 0:
+            if (arr[i] > arr[j]) == reverse:
+                arr[i], arr[j] = arr[j], arr[i]
+                j -= 1
+                i -= 1
+            else:
+                flag = 1
+
+
+
+
+
+
--- merge.py
+++ merge.py
@@ -1,3 +1,16 @@
 # merge_sort
 
-import random
+
+def merge(arr1: list, arr2: list, up=True) -> list:
+    i = 0
+    j = 0
+    n1 = len(arr1)
+    n2 = len(arr2)
+    arr = []
+    if (arr1[i] > arr[j]) == up:
+        arr.append(arr1[i])
+    else:
+        arr.append(arr2[j])
+        
+
+
